cc3f1fdbf6b18d749731e6928c6e7f4c5f3bb745
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-10 20:58:39 +0200

    refactor: remove center style

--- Reown/App.js
+++ Reown/App.js
@@ -1,22 +1,13 @@
-import { useState } from "react";
-import { Alert, Button, SafeAreaView, StyleSheet, Text } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  View
+} from "react-native";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const onPress = () => setCount(count + 1);
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native!</Text>
-      <Button title="Click Me" onPress={onPress} />
-      <Text>{count || null}</Text>
-      <Button title="Simple Alert" onPress={() => alert("Button Tapped")} />
-      <Button
-        title="Advance Alert"
-        onPress={() =>
-          Alert.prompt("Title", "Message", (text) => console.log(text))
-        }
-      />
+      <View></View>
     </SafeAreaView>
   );
 }
@@ -25,7 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
