cdf0d981846d8efcaf74e0a725c662610d1a97ce
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-10 13:18:41 +0200

    feat: add rundom image from picsum

--- Reown/App.js
+++ Reown/App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, SafeAreaView, StyleSheet, Text } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, SafeAreaView, StyleSheet, Text } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello React Native!</Text>
-      <Image source={{uri: "https://picsum.photos/200/300"}} />
+      <Image
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300",
+        }}
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -14,7 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     justifyContent: "center",
     alignItems: "center",
   },
