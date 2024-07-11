e859a92f626375cdd561e7a4e14adb4425b19e6e
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-11 08:45:52 +0200

    style: exprimenting with justifyContent, alignItems and alignSelf

--- Reown/App.js
+++ Reown/App.js
@@ -5,28 +5,30 @@ export default function App() {
   return (
     <View
       style={{
-        backgroundColor: "#fff",
+        backgroundColor: "#E1E1D5",
         flex: 1,
-        flexDirection: "row"
+        flexDirection: "row", // horizontal
+        justifyContent: "space-evenly", // main
+        alignItems: "center" // secondary
       }}
     >
       <View
         style={{
-          backgroundColor: "dodgerblue",
+          backgroundColor: "#0209C6",
           width: 100,
           height: 100,
         }}
       />
       <View
         style={{
-          backgroundColor: "gold",
+          backgroundColor: "#FFD700",
           width: 100,
           height: 100,
         }}
       />
       <View
         style={{
-          backgroundColor: "tomato",
+          backgroundColor: "#D82349",
           width: 100,
           height: 100,
         }}
