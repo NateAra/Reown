e8c2c76c42a6fc9db542476578a8cc1516eb7dc6
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-11 10:12:30 +0200

    refactor: remove init code and render welcome page

--- Reown/App.js
+++ Reown/App.js
@@ -1,38 +1,8 @@
-import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
 
   return (
-    <View
-      style={{
-        backgroundColor: "#E1E1D5",
-        flex: 1,
-        flexDirection: "row", // horizontal
-        justifyContent: "center", // main
-        alignItems: "center" // secondary
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "#0209C6",
-          width: 100,
-          height: 300,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "#FFD700",
-          width: 100,
-          height: 200,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "#D82349",
-          width: 100,
-          height: 100,
-        }}
-      />
-    </View>
+    <WelcomeScreen />
   );
 }
