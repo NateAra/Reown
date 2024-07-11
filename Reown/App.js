475ea05893a6eeb22508fc9434f59545d8069fec
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-11 08:21:24 +0200

    feat: add basic properties of flex box

--- Reown/App.js
+++ Reown/App.js
@@ -5,21 +5,30 @@ export default function App() {
   const orientation = useDeviceOrientation();
 
   return (
-    <SafeAreaView style={styles.container}>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+      }}
+    >
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: "100%",
-          height: orientation === "landscape" ? "100%" : "30%",
+          flex: 1,
         }}
-      ></View>
-    </SafeAreaView>
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          flex: 2,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          flex: 1,
+        }}
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-});
