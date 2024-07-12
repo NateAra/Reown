2943b51691c54662cea6698aef36a5fdf297017d
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-12 11:08:43 +0200

    exprimenting: borders

--- Reown/App.js
+++ Reown/App.js
@@ -1,8 +1,23 @@
+import { View, StyleSheet } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-
   return (
-    <WelcomeScreen />
+    <View style={styles.container}>
+      <View style={styles.blueBox}></View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  blueBox: {
+    backgroundColor: "dodgerblue",
+    width: 100,
+    height: 100,
+  },
+});
