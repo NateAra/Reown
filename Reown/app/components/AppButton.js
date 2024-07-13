fd298e04771737a08d23500a6c25dfe9fae8134b
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-13 18:33:31 +0200

    feat: create button component for the app

---
+++ Reown/app/components/AppButton.js
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import colors from "../config/colors";
+
+function AppButton({ title, onPress, color = "primary" }) {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: colors[color] }]}
+      onPress={onPress}
+    >
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.primary,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    width: "100%",
+  },
+  text: {
+    color: colors.white,
+    fontSize: 18,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
+});
+
+export default AppButton;
