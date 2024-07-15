13e180b237ed1af94acd2d5327e32bfc1f48f248
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-15 13:32:31 +0200

    refactor: wrap list items with swipable

--- Reown/app/components/ListItem.js
+++ Reown/app/components/ListItem.js
@@ -1,26 +1,21 @@
-import {
-  Image,
-  StyleSheet,
-  TouchableHighlight,
-  View
-} from "react-native";
+import { Image, StyleSheet, TouchableHighlight, View } from "react-native";
+import Swipeable from "react-native-gesture-handler/Swipeable";
 import colors from "../config/colors";
 import AppText from "./AppText.ios";
 
-function ListItem({ image, title, subTitle, onPress }) {
+function ListItem({ image, title, subTitle, onPress, renderRightActions }) {
   return (
-    <TouchableHighlight
-      underlayColor={colors.gray85}
-      onPress={onPress}
-    >
-      <View style={styles.container}>
-        <Image style={styles.image} source={image} />
-        <View>
-          <AppText style={styles.title}>{title}</AppText>
-          <AppText style={styles.subTitle}>{subTitle}</AppText>
+    <Swipeable renderRightActions={renderRightActions}>
+      <TouchableHighlight underlayColor={colors.gray85} onPress={onPress}>
+        <View style={styles.container}>
+          <Image style={styles.image} source={image} />
+          <View>
+            <AppText style={styles.title}>{title}</AppText>
+            <AppText style={styles.subTitle}>{subTitle}</AppText>
+          </View>
         </View>
-      </View>
-    </TouchableHighlight>
+      </TouchableHighlight>
+    </Swipeable>
   );
 }
 
