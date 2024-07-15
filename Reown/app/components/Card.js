0d6fcf76f9c39bdb7e50731b06ea409c60c4ad24
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-15 09:57:23 +0200

    feat: build a card component

---
+++ Reown/app/components/Card.js
@@ -0,0 +1,40 @@
+import { Image, StyleSheet, View } from "react-native";
+import colors from "../config/colors";
+import AppText from "./AppText.ios";
+
+function Card({ title, subTitle, image }) {
+  return (
+    <View style={styles.card}>
+      <Image resizeMode="stretch" style={styles.image} source={image} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subTitle}>{subTitle}</AppText>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 15,
+    backgroundColor: colors.black,
+    marginBottom: 20,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  detailsContainer: {
+    padding: 15,
+  },
+  title: {
+    marginBottom: 7,
+  },
+  subTitle: {
+    color: colors.primary,
+    fontWeight: "bold",
+  },
+});
+
+export default Card;
