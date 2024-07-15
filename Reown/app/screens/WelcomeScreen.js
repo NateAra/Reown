02b3a18978e234122bc8e976d411ac9c134a4165
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-15 09:10:37 +0200

    refacor: buttons

--- Reown/app/screens/WelcomeScreen.js
+++ Reown/app/screens/WelcomeScreen.js
@@ -1,18 +1,17 @@
-import React from "react";
 import { Image, ImageBackground, StyleSheet, View } from "react-native";
-import colors from "../config/colors";
 import AppButton from "../components/AppButton";
 
 function WelcomeScreen(props) {
   return (
     <ImageBackground
+      blurRadius={5}
       style={styles.background}
       source={require("../assets/bg-cart.jpg")}
     >
       <Image style={styles.logo} source={require("../assets/reown-logo.png")} />
       <View style={styles.buttonsContainer}>
         <AppButton title={"Login"} onPress={() => console.log("Tapped")} />
-        <AppButton title={"Rester"} color="secondary"/>
+        <AppButton title={"Register"} color="secondary" />
       </View>
     </ImageBackground>
   );
@@ -32,7 +31,7 @@ const styles = StyleSheet.create({
   },
   buttonsContainer: {
     padding: 20,
-    width: "100%"
+    width: "100%",
   },
 });
 
