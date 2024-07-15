7a78ed0a9c282d52072efe7494c3dad1f9423a6a
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-15 18:34:01 +0200

    feat: build init user account

---
+++ Reown/app/screens/AccountScreen.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import Screen from "../components/Screen";
+import ListItem from "../components/ListItem";
+import colors from "../config/colors";
+import Icon from "../components/Icon";
+import ListItemSeparator from "../components/ListItemSeparator";
+
+const menuItems = [
+  {
+    title: "My Listings",
+    icon: {
+      name: "format-list-bulleted",
+      backgroundColor: colors.mercury,
+    },
+  },
+  {
+    title: "My Messages",
+    icon: {
+      name: "email",
+      backgroundColor: colors.mercury,
+    },
+  },
+];
+
+function AccountScreen(props) {
+  return (
+    <Screen style={styles.screen}>
+      <View style={styles.container}>
+        <ListItem
+          title={"Nate Arafayne"}
+          subTitle={"First App"}
+          image={require("../assets/nate.jpg")}
+        />
+      </View>
+      <View style={styles.container}>
+        <FlatList
+          data={menuItems}
+          keyExtractor={(menuItem) => menuItem.title}
+          ItemSeparatorComponent={ListItemSeparator}
+          renderItem={({ item }) => (
+            <ListItem
+              title={item.title}
+              IconComponent={
+                <Icon
+                  name={item.icon.name}
+                  backgroundColor={item.icon.backgroundColor}
+                />
+              }
+            />
+          )}
+        />
+      </View>
+      <View>
+        <ListItem
+          title={"Logout"}
+          IconComponent={
+            <Icon name={"logout"} backgroundColor={colors.sunflower} />
+          }
+        />
+      </View>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 20,
+  },
+});
+
+export default AccountScreen;
