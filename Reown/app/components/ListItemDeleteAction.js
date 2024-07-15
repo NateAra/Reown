f51aa9efa3a06f3b587857c5fd7f5ed85c60d90b
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-15 13:40:57 +0200

    style: add icone

--- Reown/app/components/ListItemDeleteAction.js
+++ Reown/app/components/ListItemDeleteAction.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import colors from '../config/colors';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { StyleSheet, View } from "react-native";
+import colors from "../config/colors";
 
 function ListItemDeleteAction(props) {
   return (
-    <View style={styles.container}></View>
+    <View style={styles.container}>
+      <MaterialCommunityIcons
+        name={"trash-can-outline"}
+        size={35}
+        color={colors.jetBlack}
+      />
+    </View>
   );
 }
 
@@ -12,7 +18,9 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.danger,
     width: 70,
-  }
-})
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default ListItemDeleteAction;
