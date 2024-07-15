bd826568d112e788873b08f1845a25cbd5df7104
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-15 17:50:22 +0200

    refactor: conditonal for subtitle and style

--- Reown/app/components/ListItem.js
+++ Reown/app/components/ListItem.js
@@ -3,15 +3,23 @@ import Swipeable from "react-native-gesture-handler/Swipeable";
 import colors from "../config/colors";
 import AppText from "./AppText.ios";
 
-function ListItem({ image, title, subTitle, onPress, renderRightActions }) {
+function ListItem({
+  image,
+  title,
+  subTitle,
+  onPress,
+  renderRightActions,
+  ImageComponent,
+}) {
   return (
     <Swipeable renderRightActions={renderRightActions}>
       <TouchableHighlight underlayColor={colors.gray85} onPress={onPress}>
         <View style={styles.container}>
+          {ImageComponent}
           {image && <Image style={styles.image} source={image} />}
-          <View>
+          <View style={styles.detailsContainer}>
             <AppText style={styles.title}>{title}</AppText>
-            <AppText style={styles.subTitle}>{subTitle}</AppText>
+            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
           </View>
         </View>
       </TouchableHighlight>
@@ -24,11 +32,14 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     padding: 10,
   },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
+  },
   image: {
     width: 65,
     height: 65,
     borderRadius: 35,
-    marginRight: 10,
   },
   title: {
     color: colors.mercury,
