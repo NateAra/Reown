aa95bc309013025cdb532d0cd5c8d70a41161209
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-16 09:06:20 +0200

    refactor: add veiw with style

--- Reown/app/components/Screen.js
+++ Reown/app/components/Screen.js
@@ -1,10 +1,12 @@
 import Constants from "expo-constants";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 import colors from "../config/colors";
 
 function Screen({ children, style }) {
   return (
-    <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>
+    <SafeAreaView style={[styles.screen, style]}>
+      <View style={style}>{children}</View>
+    </SafeAreaView>
   );
 }
 
@@ -12,7 +14,7 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: colors.darkMoon
+    backgroundColor: colors.darkMoon,
   },
 });
 
