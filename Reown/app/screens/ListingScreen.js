0b2bcaae72f5387806e98bc36a5f60c129c138fe
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-16 09:06:54 +0200

    feat: build a listings Screen

---
+++ Reown/app/screens/ListingScreen.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Screen from "../components/Screen";
+import { FlatList, StyleSheet } from "react-native";
+import Card from "../components/Card";
+
+const listings = [
+  {
+    id: 1,
+    title: "Jacket for sale",
+    price: 100,
+    image: require("../assets/jacket.jpg"),
+  },
+  {
+    id: 2,
+    title: "Plane long sleeve T-Shirt",
+    price: 50,
+    image: require("../assets/longSleeve.jpg"),
+  },
+  {
+    id: 3,
+    title: "Adidas Continental 80 sneakers",
+    price: 60,
+    image: require("../assets/adidas.jpg"),
+  },
+];
+
+function ListingScreen(props) {
+  return (
+    <Screen style={styles.screen}>
+      <FlatList
+        data={listings}
+        keyExtractor={(listing) => listing.id.toString()}
+        renderItem={({ item }) => (
+          <Card title={item.title} subTitle={"$" + item.price} image={item.image} />
+        )}
+      />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 20,
+  }
+})
+
+export default ListingScreen;
