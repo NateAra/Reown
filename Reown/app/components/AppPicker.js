42cb9874c6d89a202283564696dc19a42aa40455
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-16 14:28:24 +0200

    feat: add a modal with list of catogery

--- Reown/app/components/AppPicker.js
+++ Reown/app/components/AppPicker.js
@@ -1,15 +1,46 @@
-import { StyleSheet, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  Modal,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import colors from "../config/colors";
 import AppText from "./AppText.ios";
 import Icon from "./Icon";
+import { useState } from "react";
+import Screen from "./Screen";
+import PickerItem from "./PickerItem";
 
-function AppPicker({ icon, placeholder, ...otherProps }) {
+function AppPicker({ icon, placeholder, items }) {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
-    <View style={styles.container}>
-      {icon && <Icon name={icon} size={40} iconColor={colors.darkMoon} />}
-      <AppText style={styles.text}>{placeholder}</AppText>
-      <Icon name={"menu-down"} size={40} />
-    </View>
+    <>
+      <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+        <View style={styles.container}>
+          {icon && <Icon name={icon} size={40} iconColor={colors.darkMoon} />}
+          <AppText style={styles.text}>{placeholder}</AppText>
+          <Icon name={"menu-down"} size={40} />
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisible} animationType={"slide"}>
+        <Screen>
+          <Button title="Close" onPress={() => setModalVisible(false)} />
+          <FlatList
+            data={items}
+            keyExtractor={(item) => item.value.toString()}
+            renderItem={({ item }) => (
+              <PickerItem
+                label={item.label}
+                onPress={() => console.log(item)}
+              />
+            )}
+          />
+        </Screen>
+      </Modal>
+    </>
   );
 }
 
