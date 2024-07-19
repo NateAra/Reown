2023350caf1097a8118ccffab4a691eae139bed5
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-19 14:25:09 +0200

    fix: messeage lines

--- Reown/app/screens/MessagesScreen.js
+++ Reown/app/screens/MessagesScreen.js
@@ -8,8 +8,8 @@ import Screen from "../components/Screen";
 const initMessages = [
   {
     id: 1,
-    title: "T1",
-    description: "D1",
+    title: "What is Lorem Ipsum",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was in the 1960s with the release of sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like PageMaker including versions of Lorem Ipsum.",
     image: require("../assets/nate.jpg"),
   },
   {
