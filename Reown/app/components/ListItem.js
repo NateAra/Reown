78b9349a1fdfd7950372cf290b21ac339e9adc6d
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-19 15:53:09 +0200

    refactor: code format

--- Reown/app/components/ListItem.js
+++ Reown/app/components/ListItem.js
@@ -19,8 +19,14 @@ function ListItem({
           {IconComponent}
           {image && <Image style={styles.image} source={image} />}
           <View style={styles.detailsContainer}>
-            <AppText numberOfLines={1} style={styles.title}>{title}</AppText>
-            {subTitle && <AppText numberOfLines={2} style={styles.subTitle}>{subTitle}</AppText>}
+            <AppText numberOfLines={1} style={styles.title}>
+              {title}
+            </AppText>
+            {subTitle && (
+              <AppText numberOfLines={2} style={styles.subTitle}>
+                {subTitle}
+              </AppText>
+            )}
           </View>
           <MaterialCommunityIcons
             style={styles.icon}
