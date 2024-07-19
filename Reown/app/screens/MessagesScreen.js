ec8c2fd2d63ea53b7c0948fc2a8228dba5974b83
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-19 14:30:28 +0200

    refactor: add more real title and description

--- Reown/app/screens/MessagesScreen.js
+++ Reown/app/screens/MessagesScreen.js
@@ -8,14 +8,14 @@ import Screen from "../components/Screen";
 const initMessages = [
   {
     id: 1,
-    title: "What is Lorem Ipsum",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was in the 1960s with the release of sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like PageMaker including versions of Lorem Ipsum.",
+    title: "What is Lorem Ipsum?",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
     image: require("../assets/nate.jpg"),
   },
   {
     id: 2,
-    title: "T2",
-    description: "D2",
+    title: "Why do we use it?",
+    description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ",
     image: require("../assets/yoda.jpeg"),
   },
 ];
@@ -51,8 +51,8 @@ function MessagesScreen() {
           setMessages([
             {
               id: 2,
-              title: "T2",
-              description: "D2",
+              title: "Why do we use it?",
+              description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ",
               image: require("../assets/yoda.jpeg"),
             },
           ]);
