30d04ac230187b37b27e09a2381a142cbf4083c7
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-19 15:56:06 +0200

    refactor: add dynamic width prop, style prop and icon prop

--- Reown/app/components/forms/AppFormField.js
+++ Reown/app/components/forms/AppFormField.js
@@ -2,14 +2,17 @@ import { useFormikContext } from "formik";
 import AppTextInput from "../AppTextInput";
 import ErrorMessage from "./ErrorMessage";
 
-function AppFormField({ name, ...otherProps }) {
+function AppFormField({ name, icon, width, style, ...otherProps }) {
   const { setFieldTouched, handleChange, errors, touched } = useFormikContext();
 
   return (
     <>
       <AppTextInput
+        icon={icon}
+        style={style}
         onBlur={() => setFieldTouched(name)}
         onChangeText={handleChange(name)}
+        width={width}
         {...otherProps}
       />
       <ErrorMessage error={errors[name]} visible={touched[name]} />
