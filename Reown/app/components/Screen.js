20593e6ab0cdae72097d7b28d7baebed84f7ed18
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-19 14:33:22 +0200

    fix: the cutoff refresh of list message

--- Reown/app/components/Screen.js
+++ Reown/app/components/Screen.js
@@ -5,7 +5,7 @@ import colors from "../config/colors";
 function Screen({ children, style }) {
   return (
     <SafeAreaView style={[styles.screen, style]}>
-      <View style={style}>{children}</View>
+      <View style={[ styles.view, style]}>{children}</View>
     </SafeAreaView>
   );
 }
@@ -16,6 +16,10 @@ const styles = StyleSheet.create({
     paddingTop: Constants.statusBarHeight,
     backgroundColor: colors.darkMoon,
   },
+  view: {
+    flex: 1,
+    backgroundColor: colors.darkMoon,
+  }
 });
 
 export default Screen;
