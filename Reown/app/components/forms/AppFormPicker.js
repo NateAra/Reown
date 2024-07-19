790c644aef9a8ab04f7d1ed21b15cb2244da0383
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-19 15:55:37 +0200

    refactor: add dynamic width prop and icon prop

--- Reown/app/components/forms/AppFormPicker.js
+++ Reown/app/components/forms/AppFormPicker.js
@@ -1,18 +1,19 @@
 import { useFormikContext } from "formik";
-import React from "react";
 import AppPicker from "../AppPicker";
 import ErrorMessage from "./ErrorMessage";
 
-function AppFormPicker({ items, name, placeholder }) {
+function AppFormPicker({ items, name, placeholder, width, icon }) {
   const { setFieldValue, errors, touched, values } = useFormikContext();
 
   return (
     <>
       <AppPicker
         items={items}
+        icon={icon}
         placeholder={placeholder}
         onSelectItem={(item) => setFieldValue(name, item)}
         selectedItem={values[name]}
+        width={width}
       />
       <ErrorMessage error={errors[name]} visible={touched[name]} />
     </>
