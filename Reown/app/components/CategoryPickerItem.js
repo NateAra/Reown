402e99d19fa58faf52de52e09880b674cb243547
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-22 08:16:02 +0200

    feat: build a custom category list with icons

---
+++ Reown/app/components/CategoryPickerItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Icon from "./Icon";
+import AppText from "./AppText.ios";
+import colors from "../config/colors";
+
+function CategoryPickerItem({ onPress, item }) {
+  return (
+    <View style={styles.container}>
+      <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80} />
+      <AppText style={styles.label}>{item.label}</AppText>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    alignItems: "center",
+    width: "33%",
+  },
+  label: {
+    marginTop: 10,
+    textAlign: "center",
+    color: colors.mercury
+  },
+});
+
+export default CategoryPickerItem;
