20b4ba0556d3ec25162a7c6f9ae110622866ac8f
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-22 21:18:23 +0200

    refactor: integrate auth navigation

--- Reown/App.js
+++ Reown/App.js
@@ -1,36 +1,14 @@
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Screen from "./app/components/Screen";
-import { Text, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import colors from "./app/config/colors";
-
-const Tweets = () => (
-  <Screen style={styles.backgroundColor}>
-    <Text>Tweets</Text>
-  </Screen>
-);
-
-const TweetDetails = () => (
-  <Screen style={styles.backgroundColor}>
-    <Text>Tweet Details</Text>
-  </Screen>
-);
-
-const Stack = createNativeStackNavigator();
-
-const StackNavigator = () => (
-  <Stack.Navigator initialRouteName="Tweets">
-    <Stack.Screen name="Tweets" component={Tweets} />
-    <Stack.Screen name="TweetDetails" component={TweetDetails} />
-  </Stack.Navigator>
-);
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NavigationContainer>
-        <StackNavigator />
+        <AuthNavigator />
       </NavigationContainer>
     </GestureHandlerRootView>
   );
@@ -38,6 +16,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   backgroundColor: {
-    backgroundColor: colors.mercury
-  }
-})
+    backgroundColor: colors.mercury,
+  },
+});
