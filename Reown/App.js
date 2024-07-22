ab31101784b37966b8afc07cb2a7fb5339551551
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-22 17:17:11 +0200

    Working with listing edit component

--- Reown/App.js
+++ Reown/App.js
@@ -1,27 +1,11 @@
-import { useState } from "react";
-import { StyleSheet } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import LoginScreen from "./app/screens/LoginScreen";
-import AppButton from "./app/components/AppButton";
-import Screen from "./app/components/Screen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingScreen from "./app/screens/ListingScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      {/* <WelcomeScreen /> */}
-      {/* <LoginScreen /> */}
-      {/* <AccountScreen /> */}
-      {/* <ListingScreen /> */}
-      {/* <ViewImageScreen /> */}
-      {/* <ListingDetailsScreen /> */}
-      {/* <MessagesScreen /> */}
+      <ListingEditScreen />
     </GestureHandlerRootView>
   );
 }
-
+ 
