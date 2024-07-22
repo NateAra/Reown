5b4739125110214937bfa1b7c7995a7bd0a80253
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-22 08:18:22 +0200

    refactor: integrated the custom category list

--- Reown/app/components/forms/AppFormPicker.js
+++ Reown/app/components/forms/AppFormPicker.js
@@ -2,7 +2,7 @@ import { useFormikContext } from "formik";
 import AppPicker from "../AppPicker";
 import ErrorMessage from "./ErrorMessage";
 
-function AppFormPicker({ items, name, placeholder, width, icon }) {
+function AppFormPicker({ items, name, numberOfColumns, PickerItemComponent, placeholder, width, icon }) {
   const { setFieldValue, errors, touched, values } = useFormikContext();
 
   return (
@@ -10,7 +10,9 @@ function AppFormPicker({ items, name, placeholder, width, icon }) {
       <AppPicker
         items={items}
         icon={icon}
+        numberOfColumns={numberOfColumns}
         placeholder={placeholder}
+        PickerItemComponent={PickerItemComponent}
         onSelectItem={(item) => setFieldValue(name, item)}
         selectedItem={values[name]}
         width={width}
