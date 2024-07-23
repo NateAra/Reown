a03bc02067609501778fa5bedad1619c4be9d7d5
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-23 14:36:25 +0200

    working with navigation

--- Reown/App.js
+++ Reown/App.js
@@ -3,12 +3,15 @@ import { StyleSheet } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import colors from "./app/config/colors";
 import AuthNavigator from "./app/navigation/AuthNavigator";
+import myTheme from "./app/navigation/NavTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <AuthNavigator />
+      <NavigationContainer theme={myTheme}>
+        <AppNavigator />
+        {/* <AuthNavigator /> */}
       </NavigationContainer>
     </GestureHandlerRootView>
   );
