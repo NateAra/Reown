f66473c33ea5be866c3e39df336233f9fe818089
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-23 10:19:06 +0200

    feat: build bottom tab navigation for the app

---
+++ Reown/app/navigation/AppNavigator.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import AccountScreen from "../screens/AccountScreen";
+import ListingEditScreen from "../screens/ListingEditScreen";
+import ListingScreen from "../screens/ListingScreen";
+import FeedNavigator from "./FeedNavigator";
+
+const Tab = createBottomTabNavigator();
+
+const AppNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Feed" component={FeedNavigator} options={{ headerShown: false }} />
+    <Tab.Screen name="ListingEdit" component={ListingEditScreen} />
+    <Tab.Screen name="Account" component={AccountScreen} />
+  </Tab.Navigator>
+);
+
+export default AppNavigator;
