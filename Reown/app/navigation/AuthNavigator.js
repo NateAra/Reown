1ffc92aa732fa776e34d9f66bdcf6afdae6aac34
NateAra <nate.arafayne@appliedtechnology.se>
2024-07-23 08:27:47 +0200

    style: add header style to navigation

--- Reown/app/navigation/AuthNavigator.js
+++ Reown/app/navigation/AuthNavigator.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import colors from "../config/colors";
 import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
 import WelcomeScreen from "../screens/WelcomeScreen";
@@ -7,20 +8,21 @@ import WelcomeScreen from "../screens/WelcomeScreen";
 const Stack = createNativeStackNavigator();
 
 const AuthNavigator = () => (
-  <Stack.Navigator>
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: colors.darkMoonLight,
+      },
+      headerTintColor: colors.mercury,
+    }}
+  >
     <Stack.Screen
       name="Welcome"
       component={WelcomeScreen}
       options={{ headerShown: false }}
     />
-    <Stack.Screen
-      name="Login"
-      component={LoginScreen}
-    />
-    <Stack.Screen
-      name="Register"
-      component={RegisterScreen}
-    />
+    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="Register" component={RegisterScreen} />
   </Stack.Navigator>
 );
 
